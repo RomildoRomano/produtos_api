826cec59ea528c653ae69f0477064d4b23197c09
romildolopes174@gmail.com <romildolopes174@gmail.com>
2023-05-24 14:07:14 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/dtos/ProdutoPostDto.java
@@ -0,0 +1,23 @@
+package br.com.cotiinformatica.dtos;
+
+import lombok.AllArgsConstructor;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProdutoPostDto {
+
+	
+	private String nome;
+	private String descricao;
+	private Double preco;
+	private Integer quantidade;
+	
+}
